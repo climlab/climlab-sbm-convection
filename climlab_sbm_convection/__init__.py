f3aa966df14e469d390facf33b414d2e66ca3191
Brian Rose <brose@albany.edu>
2024-07-30 23:42:27 -0400

    Reorganize into proper package and first test

---
+++ climlab_sbm_convection/__init__.py
@@ -0,0 +1,4 @@
+__version__ = '0.1'
+
+#  import everything from the fortran object
+from ._simplified_betts_miller import *
---
+++ climlab_sbm_convection/tests/test_sbm_convection.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pytest
+from climlab_sbm_convection import betts_miller, escomp
+
+
+def test_saturation():
+    temperature = 280.  # in Kelvin
+    es = escomp(temperature)/100.  # convert to hPa
+    #  relative tolerance for these tests ...
+    tol = 1E-5
+    assert es == pytest.approx(9.91189, rel=tol)
